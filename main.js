7240718693f3b4ae37e3b796d1f64daf94c89072
MrWamG <wangliamgcom@gmail.com>
2021-03-19 14:09:34 +0800

    update

---
+++ main.js
@@ -0,0 +1,19 @@
+var roleHarvester = require('harvester');
+let roleUpgrader = require('role.upgrader');
+module.exports.loop = function () {
+    var harvesters = _.filter(Game.creeps, (creep) => creep);
+
+    if(harvesters.length < 20) {
+        var newName = 'Harvester' + Game.time;
+        Game.spawns['MrWamG'].spawnCreep([WORK,CARRY,MOVE], newName, 
+            {memory: {role: 'harvester'}});
+    }
+    for(let i = 0;i<harvesters.length && i < 5;i++) {
+        let creep = harvesters[i];
+        roleHarvester.run(creep);
+    }
+    for(let i = 5;i<harvesters.length;i++) {
+        let creep = harvesters[i];
+        roleUpgrader.run(creep);
+    }
+}
