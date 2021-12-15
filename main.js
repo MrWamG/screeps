b891bdf5b1b0d59e26784387e285d6bd02054e97
MrWamG <wangliamgcom@gmail.com>
2021-12-15 09:37:44 +0800

    解决了多房间情况下二矿Spawn不孵化creep、creep不工作的问题

--- main.js
+++ main.js
@@ -2,73 +2,140 @@ const roleUpgrader = require('role.upgrader'); // 升级控制器
 const roleBuilder = require('role.builder'); // 建筑
 const roleExtension = require('role.extension'); // 运输能量至虫巢或扩容器
 const roleClaim = require('role.claim'); // 占领 
-const methods = require('methods');// 方法集合
+const methods = require('methods'); // 方法集合
 const structure_list = require('structure_list'); // 将所有存在过的建筑存放在room的memory中，当建筑不存在后将自动创造工地
 const tower = require('tower'); // 防御塔功能运行
 module.exports.loop = function () {
     structure_list.run();
     let creepArr = _.filter(Game.creeps, (creep) => creep);
-    
+    if (Game.cpu.bucket >= 10000) {
+        Game.cpu.generatePixel();
+    }
     // 删除无用的creep内存
-    for(let name in Memory.creeps){
-        if(!Game.creeps[name]){
+    for (let name in Memory.creeps) {
+        if (!Game.creeps[name]) {
             delete Memory.creeps[name];
         }
     }
-    
+
     // 每个房间的循环
-    for(let room_index in Game.rooms){
+    for (let room_index in Game.rooms) {
         let room = Game.rooms[room_index];
 
         // 敌人集合
-        let enemys = room.find(FIND_CREEPS,{
-            filter:item=>{
+        let enemys = room.find(FIND_CREEPS, {
+            filter: item => {
                 return !item.my
             }
         });
         // 房间中用于孵化的能量总值
         let roomEnergy = methods.getSpawnEnergy(room);
-        // 最大的工作部件，最低不得小于1，200为1 carry + 1 move + 生产creep本身需要的100能量
-        let maxWork = Math.max(1,Math.floor((roomEnergy - 200) / BODYPART_COST['work']));
+
+        /** 当前房间的孵化器
+         * @type {Array}
+         */
+        let spawn = room.find(FIND_MY_SPAWNS);
         
+        // 最大的工作部件，最低不得小于1，200为1carry + 1move + 生产creep本身需要的100能量
+        let maxWork = Math.max(1, Math.floor((roomEnergy - 200) / BODYPART_COST['work']));
+        console.log('maxWork', maxWork);
         // Tower防御行为运转中
-        tower.run(room,enemys);
+        tower.run(room, enemys);
 
         console.log(room.name + '中可用于孵化的能量有:' + roomEnergy)
-        methods.role_spawn({
-            role_name:'harvester',
-            spawn_name:'Spawn1',
-            num:2,
-            body_json:{'work':maxWork,'carry':1,'move':1}
-        });
-        
-        methods.role_spawn({
-            role_name:'upgrade',
-            spawn_name:'Spawn1',
-            num:5,
-            body_json:{'work':maxWork,'carry':1,'move':1}
-        });
-        
-        methods.role_spawn({
-            role_name:'builder',
-            spawn_name:'Spawn1',
-            num:2,
-            body_json:{'work':1,'carry':1,'move':1}
-        });
-        
+
+        // 须要孵化的creep身份及其部件配置
+        let role_spawn_arr = [
+            {
+                role_name: 'harvester',
+                spawn:spawn[0],
+                num: 2,
+                body_json: {
+                    'work': 1,
+                    'carry': 1,
+                    'move': 1
+                }
+            },{
+                role_name: 'upgrade',
+                spawn:spawn[0],
+                num: 4,
+                body_json: {
+                    'work': 3,
+                    'carry': 2,
+                    'move': 2
+                }
+            },{
+                role_name: 'builder',
+                spawn:spawn[0],
+                num: 2,
+                body_json: {
+                    'work': 1,
+                    'carry': 1,
+                    'move': 1
+                }
+            }
+        ];
+
+        // 须要生产的基础creep数量
+        let base_creep_num = role_spawn_arr.reduce((total,item)=>{
+            return total + item.num
+        },0)
+
+        console.log('base_creep_num',base_creep_num)
+        // 孵化creep
+        if(room.name == 'W41S54'){
+            for(let i in role_spawn_arr){
+                methods.role_spawn(role_spawn_arr[i]);
+            }
+        }else if(room.name == 'W42S54'){
+            methods.role_spawn({
+                role_name: 'harvester',
+                spawn:spawn[0],
+                num: 2,
+                body_json: {'work': 1,'carry': 1,'move': 1}
+            });
+            methods.role_spawn({
+                role_name: 'upgrade',
+                spawn:spawn[0],
+                num: 2,
+                body_json: {'work': 1,'carry': 1,'move': 1}
+            });
+        }
         for (let i = 0; i < creepArr.length; i++) {
             let creep = creepArr[i];
-            if(creep.memory.role === 'harvester'){
-                roleExtension.run(creep,1);
-            }else if(creep.memory.role === 'upgrade'){
+            if (creep.memory.role === 'harvester') {
+                if(room.name == 'W41S54'){
+                    roleExtension.run(creep, 1);
+                }else if(room.name == 'W42S54'){
+                    roleExtension.run(creep, 0);
+                }
+            } else if (creep.memory.role === 'upgrade') {
                 roleUpgrader.run(creep);
-            }else if(creep.memory.role === 'builder'){
-                roleBuilder.run(creep,1);
-                roleExtension.run(creep,1);
+            } else if (creep.memory.role === 'builder') {
+                if(room.name == 'W41S54'){
+                    roleBuilder.run(creep, 1, room);
+                }else if(room.name == 'W42S54'){
+                    roleExtension.run(creep, 0, room);
+                }
+            } else if (creep.memory.role === 'claim') {
+                roleClaim.run(creep,'W42S54')
             }
         }
-        
-    }// 每个房间的循环
 
+        // 只有当基础creep的数量大于等于须要生产的基础creep数量时才会生产claimer
+        // 我毕竟须要保障我当前基地的基本运营
+        // if(creepArr.filter(item=>{return item.memory.role !== 'claim'}).length >= base_creep_num){
+        //     methods.role_spawn({
+        //         role_name: 'claim',
+        //         spawn_name: 'Spawn1',
+        //         num: 1,
+        //         body_json: {
+        //             'claim': 1,
+        //             'move': 6
+        //         }
+        //     });
+        // }
+    } // 每个房间的循环
+    
     console.log('creeps num: ' + creepArr.length);
 }
--- method.js
+++ method.js
@@ -2,41 +2,39 @@ module.exports = {
     spawn_creeper({
         body_json,
         role_name,
-        spawn_name,
+        spawn,
         memory,
     }) {
+        console.log('spawn_creeper',spawn);
         let body_part = [];
         Object.entries(body_json).forEach(([key,value]) => body_part.push(...Array(value).fill(key)));
         let newName = role_name + Game.time;
-        if(Game.spawns[spawn_name]){
-            Game.spawns[spawn_name].spawnCreep(body_part, newName, {
-                memory,
-            });
-        }else{
-            console.log(spawn_name + '不存在');
-        }
+        spawn.spawnCreep(body_part, newName, {
+            memory,
+        });
     },
     /** 检索creeps中的身份，对每种身份进行数量要求定义每种身份所需要的身体
     * @param {Object} body_json 身体组件，传入组件的字符串而非常量
     * @param {String} role_name creep的身份名称
-    * @param {String} spawn_name 使用哪个孵化器生产creep
+    * @param {Object} spawn 使用哪个孵化器生产creep
     * @param {Number} num creep生产的最大数量
     */
     role_spawn({
         body_json = {},
         role_name = '',
-        spawn_name = '',
+        spawn = {},
         num = 0,
     }) {
+        console.log('spawn',spawn);
         let creepArr = _.filter(Game.creeps, (creep) => creep);
         let creep = creepArr.filter(item => {
-            return item.memory.role === role_name;
+            return (item.memory.role === role_name) && (item.room === spawn.room);
         })
         if (creep.length < num) {
             this.spawn_creeper({
                 body_json,
                 role_name,
-                spawn_name,
+                spawn,
                 memory: {
                     role: role_name
                 }
