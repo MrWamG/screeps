9baf9f3dc5391ac4f59b861b983d3c7c260b658b
MrWamG <wangliamgcom@gmail.com>
2021-12-17 16:46:12 +0800

    upgrader增加路线显示

--- role.upgrader.js
+++ role.upgrader.js
@@ -4,7 +4,11 @@ var roleUpgrader = {
         if (creep.memory.status == 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+                creep.moveTo(sources[0], {
+                    visualizePathStyle: {
+                        stroke: '#ffaa00'
+                    }
+                });
             }
             if (creep.carry.energy == creep.carryCapacity) {
                 creep.memory.status = 1;
@@ -14,7 +18,11 @@ var roleUpgrader = {
                 creep.memory.status = 0;
             }
             if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
+                creep.moveTo(creep.room.controller,{
+                    visualizePathStyle: {
+                        stroke: '#ffffff'
+                    }
+                });
             }
         }
     }
