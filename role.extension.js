34ac86eeb8cf4a3593702ee8649e27b6cc3972e9
MrWamG <wangliamgcom@gmail.com>
2021-12-21 09:30:33 +0800

    现在工人会优先填充基础能量建筑后再去将能量运输给storage了

--- role.extension.js
+++ role.extension.js
@@ -17,33 +17,37 @@ module.exports = {
                 creep.memory.harvest = false;
             }
         } else {
-            creep.say('transfer');
             /*creep.room.find(参数1：查找的类型,参数2：对象数组)*/
-            let targets = creep.room.find(FIND_STRUCTURES, {
-
+            let baseEnergyStructures = creep.room.find(FIND_STRUCTURES, {
                 /* 这是一个过滤器，过滤建筑，返回建筑类型是扩容器或者虫巢，条件是未满载的*/
-
                 filter: (structure) => {
-
                     return (
                         structure.structureType == STRUCTURE_EXTENSION 
                         || structure.structureType == STRUCTURE_SPAWN 
                         || structure.structureType == STRUCTURE_TOWER 
                     )
                     && structure.energy < structure.energyCapacity
-                    || structure.structureType == STRUCTURE_STORAGE;
-
                 }
-
             });
             
-            if (targets.length > 0) {
+            // 运输目标
+            let target = null;
+            // 先装满基础能量设施
+            if (baseEnergyStructures.length > 0) {
                 // 找到更近的虫巢或扩容器
-                let closerTarget = creep.pos.findClosestByRange(targets);
+                target = creep.pos.findClosestByRange(baseEnergyStructures);
+            }else{
+                // 如果基础能量设施的能量都是满的，那么就去填充storage
+                target = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {    
+                        return structure.structureType == STRUCTURE_STORAGE;
+                    }
+                })[0];
+            }
 
-                if (creep.transfer(closerTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-
-                    creep.moveTo(closerTarget, {
+            if(target){
+                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, {
                         visualizePathStyle: {
                             stroke: '#ffffff'
                         }
